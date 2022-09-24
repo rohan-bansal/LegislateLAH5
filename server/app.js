14ed99a6325ec68d67a6d9be96e9b738d01d87b7
Rohan Bansal <bansalrohan18@gmail.com>
2022-09-24 12:07:04 -0700

    test

--- server/app.js
+++ server/app.js
@@ -6,5 +6,29 @@ const logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+let setCache = function (req, res, next) {
+    if (req.method == "GET") {
+      res.set("Cache-control", `no-cache`);
+    } else {
+      res.set("Cache-control", `no-store`);
+    }
+    next();
+  };
+  
+
 app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env["CLIENT_URL"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
+
+app.use(setCache);
+
+app.use("/")
