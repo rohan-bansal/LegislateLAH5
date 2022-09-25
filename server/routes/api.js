da08c18ea213e562b9d8f6a0681d37e9898d2844
Rohan Bansal <bansalrohan18@gmail.com>
2022-09-24 17:49:33 -0700

    minor change

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,15 +4,9 @@ var fs = require("fs");
 const router = express.Router();
 
 router.post("/retrieveStancesByLegislator", function (req, res) {
-  var obj = JSON.parse(fs.readFileSync("./bills/Tariffs/result.json", "utf8"));
+//   var obj = JSON.parse(fs.readFileSync("./bills/Tariffs/result.json", "utf8"));
 
-  res.send(obj);
+  res.send("Received " + req.body);
 });
-
-router.post("/retrieveLegislatorsByDistrict", function (req, res) {});
-
-router.post("/retrieveHouseLegislators", function (req, res) {});
-
-router.post("/retrieveSenateLegislators", function (req, res) {});
 
 module.exports = router;
