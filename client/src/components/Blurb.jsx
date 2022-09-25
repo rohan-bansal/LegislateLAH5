988cad05f1dde7cf2dc03063643db702646b79a9
AndyLi23 <andyliqy@gmail.com>
2022-09-25 11:05:06 -0700

    Blurb

--- client/src/components/Blurb.jsx
+++ client/src/components/Blurb.jsx
@@ -10,7 +10,7 @@ class Blurb extends React.Component {
             <div style={{borderRadius: "50px", padding: "10px"}}>
                 <div className="text-center mx-auto lg:w-2/3 px-10 lg:px-20 py-10 rounded-2xl">
                     <h1 className="mx-auto w-fit text-2xl lg:text-5xl pb-10 pt-6 font-bold" style={{color: "#ADEFFF"}}>What Does Your Legislator Truly Think?</h1>
-                    <p className="text-base text-center lg:text-2xl text-left indent-12 leading-10">
+                    <p className="text-base text-center lg:text-2xl">
                         What legislators say on Twitter or in rallies often <span style={{textDecoration: "#ADEAFF underline", textUnderlineOffset: "3.5px"}}>doesn't align</span> with their true political views.
                         Legislators have motivation to present themselves in ways that lead to their re-election.
                         However, informed citizens should know the true sentiments of their representatives.
